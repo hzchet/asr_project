76e8832fd99290091d205f61ae8f5c8f8c3705f4
aidaramrin <aidar@cerebra.kz>
2023-10-12 20:44:16 +0600

    implement wer & cer calculation

--- src/metric/utils.py
+++ src/metric/utils.py
@@ -1,10 +1,24 @@
-# Don't forget to support cases when target_text == ''
+import editdistance
+
 
 def calc_cer(target_text, predicted_text) -> float:
-    # TODO: your code here
-    raise NotImplementedError()
+    if len(target_text) != 0:
+        return editdistance.eval(target_text, predicted_text) / len(target_text)
+    
+    if len(predicted_text) != 0:
+        return 1
+
+    return 0
 
 
 def calc_wer(target_text, predicted_text) -> float:
-    # TODO: your code here
-    raise NotImplementedError()
+    target_tokens = target_text.split()
+    predicted_tokens = predicted_text.split()
+    
+    if len(target_tokens) != 0:
+        return editdistance.eval(target_tokens, predicted_tokens) / len(target_tokens)
+    
+    if len(predicted_tokens) != 0:
+        return 1
+    
+    return 0
