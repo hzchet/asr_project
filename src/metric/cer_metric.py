2fb1a88e9519a4ffdbf2a6035e1e10ba16e2adfb
aidaramrin <aidar@cerebra.kz>
2023-10-12 22:42:13 +0600

    implement wer & cer metrics for beam search results

--- src/metric/cer_metric.py
+++ src/metric/cer_metric.py
@@ -25,3 +25,24 @@ class ArgmaxCERMetric(BaseMetric):
                 pred_text = self.text_encoder.decode(log_prob_vec[:length])
             cers.append(calc_cer(target_text, pred_text))
         return sum(cers) / len(cers)
+
+
+class BeamSearchCERMetric(BaseMetric):
+    def __init__(self, text_encoder: BaseTextEncoder, beam_size: int, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        assert hasattr(text_encoder, "ctc_beam_search")
+        self.text_encoder = text_encoder
+        self.beam_size = beam_size
+        
+    def __call__(self, log_probs: Tensor, log_probs_length: Tensor, text: List[str], **kwargs):
+        cers = []
+        lengths = log_probs_length.detach().numpy()
+        for probs, length, target_text in zip(log_probs, lengths, text):
+            target_text = BaseTextEncoder.normalize_text(target_text)
+            
+            beam_search_result = self.text_encoder.ctc_beam_search(probs, length, self.beam_size)
+            pred_text = beam_search_result[0].text
+            
+            cers.append(calc_cer(target_text, pred_text))
+        
+        return sum(cers) / len(cers)
--- src/metric/wer_metric.py
+++ src/metric/wer_metric.py
@@ -25,3 +25,24 @@ class ArgmaxWERMetric(BaseMetric):
                 pred_text = self.text_encoder.decode(log_prob_vec[:length])
             wers.append(calc_wer(target_text, pred_text))
         return sum(wers) / len(wers)
+
+
+class BeamSearchWERMetric(BaseMetric):
+    def __init__(self, text_encoder: BaseTextEncoder, beam_size: int, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        assert hasattr(text_encoder, "ctc_beam_search")
+        self.text_encoder = text_encoder
+        self.beam_size = beam_size
+        
+    def __call__(self, log_probs: Tensor, log_probs_length: Tensor, text: List[str], **kwargs):
+        wers = []
+        lengths = log_probs_length.detach().numpy()
+        for probs, length, target_text in zip(log_probs, lengths, text):
+            target_text = BaseTextEncoder.normalize_text(target_text)
+            
+            beam_search_result = self.text_encoder.ctc_beam_search(probs, length, self.beam_size)
+            pred_text = beam_search_result[0].text
+            
+            wers.append(calc_wer(target_text, pred_text))
+        
+        return sum(wers) / len(wers)
